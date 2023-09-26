e6b938759f42c050412953ea1b274e513f8e8ee3
Matej Meško <meshosk@gmail.com>
2023-09-26 14:22:50 +0200

    pridane snippety

---
+++ snippets/post.snippet.view.php
@@ -0,0 +1,14 @@
+<div class="container-fluid">
+    <div class="row justify-content-center">
+        <div class="col-3 d-flex gap-4  flex-column">
+            <div class="border post d-flex flex-column">
+                <div>
+                    <img src="https://www.pngarts.com/files/18/I-Know-That-Feel-Bro-PNG-Pic.png" class="img-fluid">
+                </div>
+                <div class="m-2">
+                    Proin vitae euismod nibh, ut tempus mi. Nunc quis vehicula lorem. Maecenas vel cursus tellus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; In quis lacinia sapien. Nam condimentum leo eu ligula maximus, sollicitudin suscipit mauris interdum.
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
