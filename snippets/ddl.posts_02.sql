d14f803e31139e61bf06f9d5e15385b10c61be8c
Matej Meško <meshosk@gmail.com>
2023-10-05 08:23:21 +0200

    pridanie DDL skriptov

---
+++ snippets/ddl.posts_02.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `posts` (
+                         `id` int(11) NOT NULL AUTO_INCREMENT,
+                         `text` text DEFAULT NULL,
+                         `picture` varchar(300) NOT NULL,
+                         `user` text NOT NULL DEFAULT 'Anonym',
+                         PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
